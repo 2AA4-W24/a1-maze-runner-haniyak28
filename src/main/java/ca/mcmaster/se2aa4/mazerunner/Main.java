c73bf3e93ea5e2524d7372e1ede4328644b50ac8
Haniya Kashif <haniya.kashif2801@gmail.com>
2024-01-24 19:52:31 -0500

    Refactor code to accept input file using -i or --input option

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -12,9 +12,32 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("** Starting Maze Runner");
+
+        // Default input file
+        String inputFile = null;
+
+        // Parse command line options
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-i") || args[i].equals("--input")) {
+                // Check if there is another argument after the option
+                if (i + 1 < args.length) {
+                    inputFile = args[i + 1];
+                    break;
+                } else {
+                    System.err.println("Error: Missing argument for input file option.");
+                    return;
+                }
+            }
+        }
+
+        if (inputFile == null) {
+            System.err.println("Error: Input file not specified. Use -i or --input option to specify the input file.");
+            return;
+        }
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            System.out.println("**** Reading the maze from file " + inputFile);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -26,9 +49,10 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+        } catch (Exception e) {
+            System.err.println("/!\\ An error has occurred /!\\");
         }
+
         System.out.println("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
         System.out.println("** End of MazeRunner");
